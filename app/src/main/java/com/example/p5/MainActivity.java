c65df7ca14e1a673b1c7abdffcc1b6fbc4742177
mhandlerSWE <max.handler@rutgers.edu>
2022-11-29 13:15:41 -0500

    first commit

---
+++ app/src/main/java/com/example/p5/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.p5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hello world");
+    }
+}
